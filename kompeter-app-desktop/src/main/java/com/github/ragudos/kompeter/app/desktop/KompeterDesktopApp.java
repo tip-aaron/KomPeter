302ad903f6921cf9fdf875159e260e9c27e10449
Ragudos <workemailaaronragudos@gmail.com>
2025-09-18 21:26:25 +0800

    refactor: desktop app

--- kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/KompeterDesktopApp.java
+++ kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/KompeterDesktopApp.java
@@ -2,6 +2,6 @@ package com.github.ragudos.kompeter.app.desktop;
 
 public class KompeterDesktopApp {
 	public static void main(String[] args) {
-		System.out.println("Hello, KomPeeeeeteeer!");
+		System.out.println("Hello, KomPeeeeeeeeteeer!");
 	}
 }
