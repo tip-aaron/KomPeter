89ae328c8a13e32317b5579863837649e1071ba6
tip-aaron <qaamragudos@tip.edu.ph>
2025-09-18 21:59:17 +0800

    style: fix formatting errors

--- kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/KompeterDesktopApp.java
+++ kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/KompeterDesktopApp.java
@@ -1,7 +1,7 @@
 package com.github.ragudos.kompeter.app.desktop;
 
 public class KompeterDesktopApp {
-	public static void main(String[] args) {
-		System.out.println("Hello, KomPeteeer!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, KomPeteeer!");
+    }
 }
