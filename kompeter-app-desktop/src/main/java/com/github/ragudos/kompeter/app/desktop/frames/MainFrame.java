5f7a8356a4414bef91cc7d27b0df22df9639683c
tip-aaron <qaamragudos@tip.edu.ph>
2025-09-22 19:54:10 +0800

    feat: add base app starter
    - add a basic app startup operations
    - add a global uncaught exception handler
    - add a base main frame
    
    Signed-off-by: Aaron Ragudos, 2025-09-22 19:54:10

---
+++ kompeter-app-desktop/src/main/java/com/github/ragudos/kompeter/app/desktop/frames/MainFrame.java
@@ -0,0 +1,28 @@
+package com.github.ragudos.kompeter.app.desktop.frames;
+
+import com.github.ragudos.kompeter.utilities.constants.Metadata;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    private class MainFrameWindowListener extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            // TODO: Cleanup operations
+            dispose();
+        }
+    }
+
+    private final MainFrameWindowListener windowListener = new MainFrameWindowListener();
+
+    public MainFrame() {
+        addWindowListener(windowListener);
+
+        setTitle(Metadata.APP_TITLE + " " + Metadata.APP_VERSION);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+}
