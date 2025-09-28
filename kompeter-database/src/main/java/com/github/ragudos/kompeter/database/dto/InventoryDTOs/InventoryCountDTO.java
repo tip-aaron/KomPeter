e2888980673e8cbdbe4170af58f2069bcdb7d7db
Hanzm10 <qhlmmapua@tip.edu.ph>
2025-09-28 22:23:14 +0800

    refactor(database): added fields, constructors, and getters in InventoryCountDTO and InventoryValueDTO

--- kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dto/InventoryDTOs/InventoryCountDTO.java
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dto/InventoryDTOs/InventoryCountDTO.java
@@ -4,10 +4,52 @@
  */
 package com.github.ragudos.kompeter.database.dto.InventoryDTOs;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author Hanz Mapua
  */
 public class InventoryCountDTO {
+    public Timestamp date;
+    public int totalInventoryCount;
+    public int totalPurchaseCount;
+    public int totalSalesCount;
+
+    public InventoryCountDTO(Timestamp date, int totalInventoryCount, int totalPurchaseCount, int totalSalesCount) {
+        this.date = date;
+        this.totalInventoryCount = totalInventoryCount;
+        this.totalPurchaseCount = totalPurchaseCount;
+        this.totalSalesCount = totalSalesCount;
+    }
+
+    public InventoryCountDTO(Timestamp date, int totalInventoryCount) {
+        this.date = date;
+        this.totalInventoryCount = totalInventoryCount;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public int getTotalInventoryCount() {
+        return totalInventoryCount;
+    }
+
+    public int getTotalPurchaseCount() {
+        return totalPurchaseCount;
+    }
+
+    public int getTotalSalesCount() {
+        return totalSalesCount;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryCountDTO{" + "date=" + date + ", totalInventoryCount=" + totalInventoryCount + ", totalPurchaseCount=" + totalPurchaseCount + ", totalSalesCount=" + totalSalesCount + '}';
+    }
+    
+    
+    
     
 }
--- kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dto/InventoryDTOs/InventoryValueDTO.java
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dto/InventoryDTOs/InventoryValueDTO.java
@@ -4,10 +4,50 @@
  */
 package com.github.ragudos.kompeter.database.dto.InventoryDTOs;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author Hanz Mapua
  */
 public class InventoryValueDTO {
+    public Timestamp date;
+    public float totalInventoryValue;
+    public float totalPurchaseValue;
+    public float totalSalesValue;
+
+    public InventoryValueDTO(Timestamp date, float totalInventoryValue, float totalPurchaseValue, float totalSalesValue) {
+        this.date = date;
+        this.totalInventoryValue = totalInventoryValue;
+        this.totalPurchaseValue = totalPurchaseValue;
+        this.totalSalesValue = totalSalesValue;
+    }
+
+    public InventoryValueDTO(Timestamp date, float totalInventoryValue) {
+        this.date = date;
+        this.totalInventoryValue = totalInventoryValue;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public float getTotalInventoryValue() {
+        return totalInventoryValue;
+    }
+
+    public float getTotalPurchaseValue() {
+        return totalPurchaseValue;
+    }
+
+    public float getTotalSalesValue() {
+        return totalSalesValue;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryValueDTO{" + "date=" + date + ", totalInventoryValue=" + totalInventoryValue + ", totalPurchaseValue=" + totalPurchaseValue + ", totalSalesValue=" + totalSalesValue + '}';
+    }
+    
     
 }
