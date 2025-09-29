221337e9b757b0eb768387ca2812446d736e1a48
tip-aaron <qaamragudos@tip.edu.ph>
2025-09-30 03:19:20 +0800

    add more to dao

---
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/sqlite/dao/SqliteItemCategoryAssignmentDao.java
@@ -0,0 +1,6 @@
+package com.github.ragudos.kompeter.database.sqlite.dao;
+
+import com.github.ragudos.kompeter.database.dao.ItemCategoryAssignmentDao;
+
+public class SqliteItemCategoryAssignmentDao implements ItemCategoryAssignmentDao {
+}
