cde9574d6d1bd234017c38d5edc84dcce76f740e
familyaccount <familyaccount@localhost>
2025-10-06 00:03:21 +0800

    refactor: itemDto

--- kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dto/ItemDto.java
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dto/ItemDto.java
@@ -4,4 +4,44 @@ import java.sql.Timestamp;
 import org.jetbrains.annotations.NotNull;
 
 public record ItemDto(
-        int _itemId, @NotNull Timestamp _createdAt, @NotNull String name, String description) {}
+        int _itemId, @NotNull Timestamp _createdAt, @NotNull String name, String description) {
+	
+	public static class Builder {
+		private int itemId;
+		private Timestamp createdAt;
+		private String name;
+		private String description;
+		
+		public @NotNull ItemDto build() throws IllegalStateException{
+			if (itemId <= 0) {
+				throw new IllegalStateException("Item ID must be greater than 0");
+			}
+			if (createdAt == null) {
+				throw new IllegalStateException("Item creation time cannot be null");
+			}
+			if (name == null || name.isBlank()) {
+				throw new IllegalStateException("Display name cannot be null or empty");
+			}
+			return new ItemDto(itemId, createdAt, name, description);
+		}
+		
+		public Builder setCreatedAt(Timestamp createdAt) {
+			this.createdAt = createdAt;
+			return this;
+		}
+		public Builder setDescription(String description) {
+			this.description = description;
+			return this;
+		}
+		public Builder setItemId(int itemId) {
+			this.itemId = itemId;
+			return this;
+		}
+		public Builder setName(String name) {
+			this.name = name;
+			return this;
+		}
+	}
+	
+	
+}
