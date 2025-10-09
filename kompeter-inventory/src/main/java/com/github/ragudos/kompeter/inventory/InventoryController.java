5d8f4feb59b6e5cf15fa999943d410ef0199ab83
Peter Dela Cruz <familyaccount@localhost>
2025-10-09 22:45:23 +0800

    add inventory service

--- kompeter-inventory/src/main/java/com/github/ragudos/kompeter/inventory/InventoryController.java
+++ kompeter-inventory/src/main/java/com/github/ragudos/kompeter/inventory/InventoryController.java
@@ -1,5 +1,25 @@
 package com.github.ragudos.kompeter.inventory;
 
+import com.github.ragudos.kompeter.database.dto.ItemDto;
+import java.util.List;
+
 public class InventoryController {
+    private final InventoryService is;
+    private final InventoryView iv;
+    
+    public InventoryController(InventoryService is, InventoryView iv){
+        this.is = is;
+        this.iv = iv;
+    }
+    
+    public void loadItems(){
+        try{
+            List<ItemDto> items = is.getAllItem();
+            
+                    
+        }catch(RuntimeException e){
+            
+        }
+    }
 
 }
