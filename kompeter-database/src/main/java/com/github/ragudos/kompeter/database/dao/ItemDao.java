6f70281277fe519f846e8d70d43381b6a60bb5b5
Peter Dela Cruz <familyaccount@localhost>
2025-10-09 22:49:04 +0800

    refactor: ItemDao

--- kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dao/ItemDao.java
+++ kompeter-database/src/main/java/com/github/ragudos/kompeter/database/dao/ItemDao.java
@@ -1,3 +1,17 @@
 package com.github.ragudos.kompeter.database.dao;
 
-public interface ItemDao {}
+import com.github.ragudos.kompeter.database.dto.ItemDto;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ItemDao {
+    //CREATE
+    //READ
+    List<ItemDto> getAllItems() throws SQLException, IOException;
+    List<ItemDto> getItemsById(int id) throws SQLException, IOException;
+    
+    
+    //UPDATE
+    //DELETE
+}
