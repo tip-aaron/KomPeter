b034156208943d0b68acf91c8a64a292ad342906
Peter Dela Cruz <familyaccount@localhost>
2025-10-09 22:47:49 +0800

    added select all items sql query

---
+++ kompeter-database/src/main/resources/com/github/ragudos/kompeter/database/sql/sqlite/select/items/select_all_items.sql
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Peter M. Dela Cruz
+ * Created: Oct 9, 2025
+ */
+
+SELECT * FROM items;
